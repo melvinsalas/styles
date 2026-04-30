03e3fcf309073c8c8a0b17d01f7968672eb56d52
Melvin Salas <melvin.salas@es.schunk.com>
2026-04-30 14:37:39 +0200

    Update stylesheet paths to be dynamic based on script location

--- loader.js
+++ loader.js
@@ -1,12 +1,15 @@
 const section = window.location.pathname.split('/').filter(Boolean)[0];
 
+const scriptSrc = document.currentScript.src;
+const base = scriptSrc.substring(0, scriptSrc.lastIndexOf('/')) + '/styles';
+
 const styles = {
-  notes: "/styles/notes.css",
-  travel: "/styles/travel.css",
-  about: "/styles/about.css"
+  notes: `${base}/notes.css`,
+  travel: `${base}/travel.css`,
+  about: `${base}/about.css`
 };
 
-const href = styles[section] || "/styles/default.css";
+const href = styles[section] || `${base}/default.css`;
 
 const link = document.createElement("link");
 link.rel = "stylesheet";
